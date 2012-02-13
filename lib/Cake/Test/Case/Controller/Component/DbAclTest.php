ac8afa1ac167c47daa08a2c610ac9c55bb9e4b35
Ceeram <c33ram@gmail.com>
2012-02-13 14:58:43 +0100

    add classes for autoloader to avoid fatal when running as individual test

--- lib/Cake/Test/Case/Controller/Component/DbAclTest.php
+++ lib/Cake/Test/Case/Controller/Component/DbAclTest.php
@@ -16,7 +16,8 @@
  * @since         CakePHP(tm) v 2.0
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-
+App::uses('ComponentCollection', 'Controller');
+App::uses('AclComponent', 'Controller/Component');
 App::uses('DbAcl', 'Controller/Component/Acl');
 App::uses('AclNode', 'Model');
 
