511a10002b81ed3eb54c037de23aac2db48f2fde
Ceeram <c33ram@gmail.com>
2012-03-16 17:55:43 +0100

    noquotes again

--- travis_build.sh
+++ travis_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 WRONG="NOT RUNNING CORRECT COMMAND!"
-if [ "$TRAVIS_PHP_VERSION" = "5.2" ]; then
+if [ $TRAVIS_PHP_VERSION = "5.2" ]; then
     echo $TRAVIS_PHP_VERSION
     ./lib/Cake/Console/cake test core AllTests --exclude-group stderr
 else
