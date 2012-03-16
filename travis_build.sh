efe749fe09f2053da6d3283941f8b541018cc1ba
Michal Papis <mpapis@gmail.com>
2012-03-16 17:58:58 +0100

    fix bashism

--- travis_build.sh
+++ travis_build.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 WRONG="NOT RUNNING CORRECT COMMAND!"
-if [ $TRAVIS_PHP_VERSION = "5.2" ]; then
+if [[ "$TRAVIS_PHP_VERSION" == "5.2" ]]
+then
     echo $TRAVIS_PHP_VERSION
     ./lib/Cake/Console/cake test core AllTests --exclude-group stderr
 else
