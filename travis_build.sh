309ef04048d2bdadb0574d43a278be1c85800363
Ceeram <c33ram@gmail.com>
2012-03-16 16:32:03 +0100

    remove space

--- travis_build.sh
+++ travis_build.sh
@@ -2,5 +2,5 @@
 if [ '$TRAVIS_PHP_VERSION' = '5.2' ]; then
     ./lib/Cake/Console/cake test core AllTests --exclude-group stderr
 else
-    ./lib/Cake/Console/cake test core AllTests -- stderr
+    ./lib/Cake/Console/cake test core AllTests --stderr
 fi
