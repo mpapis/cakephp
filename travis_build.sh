f1d17acd987830cc1dec646dce01acac796e6b3f
Ceeram <c33ram@gmail.com>
2012-03-16 15:16:50 +0100

    fix syntax

--- travis_build.sh
+++ travis_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ '$TRAVIS_PHP_VERSION' = '5.2' ]; then
-    then cake test core AllTests --exclude-group stderr
+    cake test core AllTests --exclude-group stderr
 else
-    then cake test core AllTests -- stderr
+    cake test core AllTests -- stderr
 fi
